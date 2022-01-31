8c50c4af3cce1f7bc7b49b2fd92590802bf494ea
tomtsutom <40418321+Tsutomu-Ikeda@users.noreply.github.com>
2022-01-31 22:13:24 +0900

    Update Profile.tsx

--- src/views/Profile.tsx
+++ src/views/Profile.tsx
@@ -29,27 +29,6 @@ export default function Profile() {
         <Typography variant="h6">基本情報</Typography>
         <Typography variant="body1" className={classes.sentence}>
           名前は、池田力です。力と書いてつとむと読みます。
-          <Link
-            href="https://crd.ndl.go.jp/reference/modules/d3ndlcrdentry/index.php?page=ref_view&id=1000089837"
-            target="_blank"
-            rel="noopener"
-          >
-            嘉納治五郎の力必達という言葉
-          </Link>
-          が由来らしいです。広島生まれの神奈川育ちで、広島東洋カープが好きです。
-          <br />
-        </Typography>
-
-        <Typography variant="h6">所属している組織</Typography>
-        <Typography variant="body1" className={classes.sentence}>
-          <Link
-            href="https://jp.corp-sansan.com/"
-            target="_blank"
-            rel="noopener"
-          >
-            Sansan株式会社
-          </Link>
-          でサーバーサイドエンジニアとして働いています。
         </Typography>
 
         <Typography variant="h6">趣味</Typography>
